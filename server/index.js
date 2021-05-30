9e845ae5af3d26b83bc4990c13627dd25c66a982
André Neves <andreneves3@gmail.com>
2021-05-30 16:01:05 -0300

    refactor: move main app functions

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const { createPreference } = require("./mercado-pago");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/detail", function (req, res) {
+  console.log(req.query);
+  res.render("detail", req.query);
+});
+
+app.listen(port, () => {
+  console.log(`The server is now running on Port ${port}`);
+});
